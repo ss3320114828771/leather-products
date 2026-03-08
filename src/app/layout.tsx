5b61c897052ea06a0c18c6a70ae111c5f6dfc6a7
syedsajid <sajidsyedhafizsajidsyed@gmail.com>
2026-03-08 03:33:38 -0700

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/ui/Navbar";
 import Footer from "@/components/ui/Footer";
+import HomePage from "./page";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -28,6 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <HomePage/>
         <Navbar/>
         {children}
       </body>
